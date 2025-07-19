377019c33632c9aecf1a60bf0dc60e491250292e
王伟健 <812102243@qq.com>
2025-07-20 00:05:47 +0800

    init

---
+++ pages/index/index.vue
@@ -0,0 +1,227 @@
+<template>
+	<view class="content">
+		<view class="imgs">
+			<u-swiper :list="imgs" height="1100rpx" keyName="image" showTitle :autoplay="false" circular>
+				<template v-slot="{ item }">
+					<image :src="item" style="width: 100%; height: 1100rpx; object-fit: cover;" />
+				</template>
+			</u-swiper>
+		</view>
+		<view class="detail">
+			<view class="detail-title">
+				<view class="detail-title-tips">报名中</view>
+				<view class="detail-title-title">广州东山口第一届集章大会，8月8日正式启动</view>
+			</view>
+			<view class="detail-info">
+				<view class="detail-info-top">
+					<image class="detail-info-top-img" src="/static/logo.png"></image>
+					<view class="detail-info-top-view">Starshine-js</view>
+				</view>
+				<view class="detail-info-time">
+					<image class="detail-info-time-img" src="/static/index/time.png"></image>08-08 周日 13:00~18:00
+				</view>
+				<view class="detail-info-address">
+					<view class="detail-info-address-view">
+						<image class="detail-info-address-image" src="/static/index/address.png"></image>
+						广州市越秀区天河城百货负一楼285-05
+					</view>
+					<view class="detail-info-address-right">距您 8.5km</view>
+				</view>
+				<view class="detail-info-button">
+					<view class="detail-info-button-left">3126 人已报名</view>
+					<view class="detail-info-button-right" @click="toDetail">
+						<image class="detail-info-button-right-img" src="/static/index/button.png"></image>点击报名
+					</view>
+				</view>
+			</view>
+		</view>
+		<!-- <u-button type="primary" @click="handleClick">点我</u-button> -->
+
+	</view>
+</template>
+
+<script>
+import {
+	defineComponent,
+	reactive,
+	ref,
+	watchEffect
+} from 'vue';
+export default defineComponent({
+	setup() {
+		const title = ref('ww12j')
+		const handleClick = () => {
+			console.log(112)
+		}
+		const imgs = ref(['/static/logo.png', '/static/logo.png', '/static/logo.png'])
+		const toDetail = () => {
+			console.log(11)
+			uni.navigateTo({
+				url: '/pages/detail/index?id=123'
+			});
+		}
+		return {
+			imgs,
+			title,
+			handleClick,
+			toDetail,
+		}
+	}
+});
+</script>
+
+<style>
+.detail-info-button-right-img {
+	width: 22rpx;
+	height: 24rpx;
+}
+
+.detail-info-button-right {
+	padding: 17rpx 25rpx;
+	border-radius: 100rpx;
+	background-color: #000;
+	font-weight: bold;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	gap: 7rpx;
+	color: #FFFFFF;
+	font-size: 28rpx;
+}
+
+.detail-info-button-left {
+	color: #FF4D00;
+	font-weight: bold;
+}
+
+.detail-info-button {
+	margin-top: 10rpx;
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+}
+
+.detail-info-address-right {
+	white-space: nowrap;
+}
+
+.detail-info-address-image {
+	width: 21rpx;
+	height: 24rpx;
+}
+
+.detail-info-address-view {
+	display: flex;
+	align-items: center;
+	gap: 16rpx;
+}
+
+.detail-info-address {
+	height: 60rpx;
+	margin-top: 19rpx;
+	display: flex;
+	align-items: start;
+	color: #999999;
+	justify-content: space-between;
+}
+
+.detail-info-time {
+	margin-top: 26rpx;
+	display: flex;
+	align-items: center;
+	gap: 14rpx;
+	color: #999999;
+}
+
+.detail-info-time-img {
+	width: 23rpx;
+	height: 23rpx;
+}
+
+view {
+	box-sizing: border-box;
+}
+
+image {
+	object-fit: cover;
+}
+
+.content {
+	padding-top: 80rpx;
+	background-color: #fff;
+	height: 100vh;
+	box-sizing: border-box;
+}
+
+.logo {
+	height: 200rpx;
+	width: 200rpx;
+	margin-top: 200rpx;
+	margin-left: auto;
+	margin-right: auto;
+	margin-bottom: 50rpx;
+}
+
+.imgs {
+	width: 100%;
+	height: 1100rpx;
+	background-color: #000;
+}
+
+.img {
+	width: 100%;
+	height: 1100rpx;
+}
+
+.detail {
+	position: fixed;
+	bottom: 37rpx;
+	left: 23.5rpx;
+	width: 703rpx;
+	height: 414rpx;
+	background-color: #fff;
+	border-radius: 33rpx;
+	padding: 33rpx 23rpx;
+	overflow: hidden;
+	box-sizing: border-box;
+	box-shadow: 0 4px 12px rgba(0, 0, 0, 0.1);
+
+}
+
+.detail-title {
+	display: flex;
+}
+
+.detail-title-tips {
+	background-color: #FF4D00;
+	color: #fff;
+	font-weight: 500;
+	color: #FFFFFF;
+	padding: 7rpx 20rpx;
+	white-space: nowrap;
+
+}
+
+.detail-title-title {
+	font-weight: 500;
+	color: #1A1A1A;
+}
+
+.detail-info {}
+
+.detail-info-top {
+	display: flex;
+	align-items: center;
+	gap: 16rpx;
+	margin-top: 26rpx;
+}
+
+.detail-info-top-img {
+	width: 44rpx;
+	height: 44rpx;
+}
+
+.detail-info-top-view {
+	color: #CCCCCC;
+}
+</style>
---
+++ unpackage/dist/dev/mp-weixin/pages/index/index.js
@@ -0,0 +1,65 @@
+"use strict";
+const common_vendor = require("../../common/vendor.js");
+const common_assets = require("../../common/assets.js");
+const _sfc_main = common_vendor.defineComponent({
+  setup() {
+    const title = common_vendor.ref("ww12j");
+    const handleClick = () => {
+      common_vendor.index.__f__("log", "at pages/index/index.vue:54", 112);
+    };
+    const imgs = common_vendor.ref(["/static/logo.png", "/static/logo.png", "/static/logo.png"]);
+    const toDetail = () => {
+      common_vendor.index.__f__("log", "at pages/index/index.vue:58", 11);
+      common_vendor.index.navigateTo({
+        url: "/pages/detail/index?id=123"
+      });
+    };
+    return {
+      imgs,
+      title,
+      handleClick,
+      toDetail
+    };
+  }
+});
+if (!Array) {
+  const _easycom_u_swiper2 = common_vendor.resolveComponent("u-swiper");
+  _easycom_u_swiper2();
+}
+const _easycom_u_swiper = () => "../../node-modules/uview-plus/components/u-swiper/u-swiper.js";
+if (!Math) {
+  _easycom_u_swiper();
+}
+function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
+  return {
+    a: common_vendor.w(({
+      item
+    }, s0, i0) => {
+      return {
+        a: item,
+        b: i0,
+        c: s0
+      };
+    }, {
+      name: "d",
+      path: "a",
+      vueId: "eb1e32e6-0"
+    }),
+    b: common_vendor.p({
+      list: _ctx.imgs,
+      height: "1100rpx",
+      keyName: "image",
+      showTitle: true,
+      autoplay: false,
+      circular: true
+    }),
+    c: common_assets._imports_0,
+    d: common_assets._imports_1,
+    e: common_assets._imports_2,
+    f: common_assets._imports_3,
+    g: common_vendor.o((...args) => _ctx.toDetail && _ctx.toDetail(...args))
+  };
+}
+const MiniProgramPage = /* @__PURE__ */ common_vendor._export_sfc(_sfc_main, [["render", _sfc_render]]);
+wx.createPage(MiniProgramPage);
+//# sourceMappingURL=../../../.sourcemap/mp-weixin/pages/index/index.js.map
