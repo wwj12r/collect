d5c7fceee2478a6c310c03170290625079e1e6d7
王伟健 <812102243@qq.com>
2025-07-25 11:52:36 +0800

    1

---
+++ pages/gallery/components/preview.vue
@@ -0,0 +1,132 @@
+<template>
+	<view v-if="show" class="popup-mask" @click.self="close">
+	  <view class="preview-popup">
+		<u-icon type="closeempty" size="32" color="#888" class="close-btn" @click="close" />
+		<image class="main-img" :src="imgUrl" mode="widthFix" />
+		<view class="title">{{ title }}</view>
+		<view class="desc">{{ desc }}</view>
+		<view class="sub-desc">{{ subDesc }}</view>
+		<view class="btn-row">
+		  <button class="share-btn" @click="onShare">分享</button>
+		  <button class="like-btn" @click="onLike">
+			<u-icon type="hand-up" size="22" color="#fff" style="margin-right:8rpx;" />
+			点赞
+		  </button>
+		</view>
+	  </view>
+	</view>
+  </template>
+  
+  <script setup>
+  import { defineProps, defineEmits } from 'vue'
+  
+  const props = defineProps({
+	show: Boolean,
+	imgUrl: String,
+	title: {
+	  type: String,
+	  default: 'Be Water My friend'
+	},
+	desc: {
+	  type: String,
+	  default: ''
+	},
+	subDesc: {
+	  type: String,
+	  default: "If you love life, don't waste time, for time is what life is made up of."
+	}
+  })
+  const emit = defineEmits(['update:show', 'share', 'like'])
+  
+  function close() {
+	emit('update:show', false)
+  }
+  function onShare() {
+	emit('share')
+  }
+  function onLike() {
+	emit('like')
+  }
+  </script>
+  
+  <style scoped>
+  .popup-mask {
+	position: fixed;
+	left: 0; top: 0; right: 0; bottom: 0;
+	background: rgba(0,0,0,0.55);
+	z-index: 1000;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+  }
+  .preview-popup {
+	background: #fff;
+	border-radius: 24rpx;
+	width: 88vw;
+	max-width: 480rpx;
+	padding: 32rpx 0 24rpx 0;
+	box-shadow: 0 8rpx 32rpx rgba(0,0,0,0.10);
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	position: relative;
+  }
+  .close-btn {
+	position: absolute;
+	right: 24rpx;
+	top: 24rpx;
+	z-index: 2;
+  }
+  .main-img {
+	width: 80%;
+	border-radius: 12rpx;
+	margin-bottom: 18rpx;
+  }
+  .title {
+	font-size: 28rpx;
+	font-weight: bold;
+	color: #222;
+	margin-bottom: 8rpx;
+	text-align: center;
+  }
+  .desc {
+	font-size: 24rpx;
+	color: #222;
+	margin-bottom: 4rpx;
+	text-align: center;
+  }
+  .sub-desc {
+	font-size: 20rpx;
+	color: #888;
+	margin-bottom: 18rpx;
+	text-align: center;
+  }
+  .btn-row {
+	display: flex;
+	width: 80%;
+	justify-content: space-between;
+	margin-top: 12rpx;
+  }
+  .share-btn {
+	flex: 1;
+	background: #f7f7f7;
+	color: #222;
+	font-size: 24rpx;
+	border-radius: 32rpx;
+	margin-right: 12rpx;
+	border: none;
+	padding: 14rpx 0;
+  }
+  .like-btn {
+	flex: 1;
+	background: #222;
+	color: #fff;
+	font-size: 24rpx;
+	border-radius: 32rpx;
+	border: none;
+	padding: 14rpx 0;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+  }
+  </style>
--- unpackage/dist/dev/mp-weixin/pages/gallery/index.js
+++ unpackage/dist/dev/mp-weixin/pages/gallery/index.js
@@ -1,63 +1,82 @@
 "use strict";
 const common_vendor = require("../../common/vendor.js");
-const _sfc_main = {
-  data() {
-    return {
-      cards: [
-        {
-          img: "https://your-image-url/bruce-lee.jpg",
-          title: "Be Water My friend"
-        },
-        {
-          img: "https://your-image-url/jaychou.jpg",
-          title: "咏叹不褪晓"
-        },
-        {
-          img: "https://your-image-url/slamdunk.jpg",
-          title: "SLAM DUNK"
-        },
-        {
-          img: "https://your-image-url/canton-tower.jpg",
-          title: "CANTON TOWER"
-        },
-        {
-          img: "https://your-image-url/stephen-jay.jpg",
-          title: "STEPHEN CHOW JAY CHOU"
-        },
-        {
-          img: "https://your-image-url/stamp-collection.jpg",
-          title: ""
-        }
-      ]
-    };
-  },
-  methods: {
-    onMenuClick() {
-      common_vendor.index.showToast({ title: "菜单点击", icon: "none" });
-    }
-  }
-};
 if (!Array) {
   const _component_uni_icons = common_vendor.resolveComponent("uni-icons");
   _component_uni_icons();
 }
-function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
-  return {
-    a: common_vendor.p({
-      type: "more-filled",
-      size: "32",
-      color: "#333"
-    }),
-    b: common_vendor.o((...args) => $options.onMenuClick && $options.onMenuClick(...args)),
-    c: common_vendor.f($data.cards, (card, idx, i0) => {
-      return {
-        a: card.img,
-        b: common_vendor.t(card.title),
-        c: idx
-      };
-    })
-  };
+if (!Math) {
+  StampPreviewPopup();
 }
-const MiniProgramPage = /* @__PURE__ */ common_vendor._export_sfc(_sfc_main, [["render", _sfc_render], ["__scopeId", "data-v-d6683764"]]);
+const StampPreviewPopup = () => "./components/preview.js";
+const _sfc_main = {
+  __name: "index",
+  setup(__props) {
+    const showPopup = common_vendor.ref(false);
+    const cards = common_vendor.ref([
+      {
+        img: "https://your-image-url/bruce-lee.jpg",
+        title: "Be Water My friend"
+      },
+      {
+        img: "https://your-image-url/jaychou.jpg",
+        title: "咏叹不褪晓"
+      },
+      {
+        img: "https://your-image-url/slamdunk.jpg",
+        title: "SLAM DUNK"
+      },
+      {
+        img: "https://your-image-url/canton-tower.jpg",
+        title: "CANTON TOWER"
+      },
+      {
+        img: "https://your-image-url/stephen-jay.jpg",
+        title: "STEPHEN CHOW JAY CHOU"
+      },
+      {
+        img: "https://your-image-url/stamp-collection.jpg",
+        title: ""
+      }
+    ]);
+    const onMenuClick = () => {
+      common_vendor.index.showToast({ title: "菜单点击", icon: "none" });
+    };
+    return (_ctx, _cache) => {
+      return {
+        a: common_vendor.p({
+          type: "more-filled",
+          size: "32",
+          color: "#333"
+        }),
+        b: common_vendor.o(onMenuClick),
+        c: common_vendor.f(cards.value, (card, idx, i0) => {
+          return {
+            a: card.img,
+            b: common_vendor.t(card.title),
+            c: idx,
+            d: common_vendor.o(($event) => showPopup.value = true, idx)
+          };
+        }),
+        d: common_vendor.o(($event) => _ctx.uni.showToast({
+          title: "分享",
+          icon: "none"
+        })),
+        e: common_vendor.o(($event) => _ctx.uni.showToast({
+          title: "点赞",
+          icon: "none"
+        })),
+        f: common_vendor.o(($event) => showPopup.value = $event),
+        g: common_vendor.p({
+          imgUrl: _ctx.imgUrl,
+          title: "Be Water My friend",
+          desc: "",
+          subDesc: "If you love life, don't waste time, for time is what life is made up of.",
+          show: showPopup.value
+        })
+      };
+    };
+  }
+};
+const MiniProgramPage = /* @__PURE__ */ common_vendor._export_sfc(_sfc_main, [["__scopeId", "data-v-d6683764"]]);
 wx.createPage(MiniProgramPage);
 //# sourceMappingURL=../../../.sourcemap/mp-weixin/pages/gallery/index.js.map
